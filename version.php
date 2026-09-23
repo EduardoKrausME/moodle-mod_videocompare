9bbfebcae60e84b813b65749475cc524bce95ea2
Eduardo Kraus <kraus@eduardokraus.com>
2026-09-23 15:57:38 -0300

    V 1.0.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videocompare';
-$plugin->version = 2026092000;
+$plugin->version = 2026092300;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
